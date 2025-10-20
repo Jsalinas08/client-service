b3dfd8077ff4d9355d24c0e84ecd3b0e12fab776
Jonathan Salinas <jonathan.salinas9312@gmail.com>
2025-10-20 17:17:39 -0500

    fix to inicializer Redis

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 # Load the Rails application.
 require_relative 'application'
+require 'dotenv'
+Dotenv.load
+puts "✅ Dotenv cargado, variables disponibles: #{ENV.keys.select { |k| k.match?(/REDIS|SECRET|DB|RAILS/) }}"
+
 
 # Initialize the Rails application.
 Rails.application.initialize!
